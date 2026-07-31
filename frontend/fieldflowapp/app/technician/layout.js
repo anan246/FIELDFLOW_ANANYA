6afa5cd5752b6f300ea856031e203e22dfd5c320
anan246 <anan246@users.noreply.github.com>
2026-07-31 15:41:17 +0530

    feat: align Admin, Dispatcher, Technician, and Customer settings styling and portal layout theme

--- frontend/fieldflowapp/app/technician/layout.js
+++ frontend/fieldflowapp/app/technician/layout.js
@@ -2,10 +2,10 @@ import Sidebar from "@/components/technician/Sidebar";
 
 export default function TechnicianLayout({ children }) {
   return (
-    <div className="flex min-h-screen bg-[#111111]">
+    <div className="flex min-h-screen bg-[#F4F6FB]">
       <Sidebar />
 
-      <div className="flex-1">
+      <div className="flex-1 p-6 lg:p-8 overflow-auto">
         {children}
       </div>
     </div>
